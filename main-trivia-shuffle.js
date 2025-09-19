8c1e8501d528e8f776a5d1f5f43731be8767a48c
mortenhouring <mortenhouring@gmail.com>
2025-09-19 23:19:07 +0200

    Update main-trivia-shuffle.js

--- main-trivia-shuffle.js
+++ main-trivia-shuffle.js
@@ -3,7 +3,7 @@
 // Usage: import { loadTrivia, generateTriviaParagraph } from './main-trivia-shuffle.js';
 
 let triviaData = {};
-const MAX_CHAR_DEFAULT = 270;
+const MAX_CHAR_DEFAULT = 230;
 const STOPWORDS = new Set([
   'the','a','an','and','or','in','on','of','for','to','has','have','had','is','are','with','at','by','from','over',
   'as','his','her','he','she','they','them','their','that','this','which','was','were','it','its','but','be','been',
